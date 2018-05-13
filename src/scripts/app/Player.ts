94eb492c2178dda37123b86b4e4dfa8664b6b631
Brian Rowe <browe404@gmail.com>
2018-05-12 22:44:01 -0400

    demonstrate the problem more clearly with faster bullets

--- src/scripts/app/Player.ts
+++ src/scripts/app/Player.ts
@@ -95,7 +95,7 @@ export class Player extends ActorBase {
         const bullet = this._actorFactory.createActor(ActorType.Projectile, {
             parent: this,
             rotation: this._sprite.rotation,
-            speed: 20,
+            speed: 100,
             texture: PIXI.loader.resources.bubble.texture,
             scale: new PIXI.Point(0.3, 0.3),
         });
